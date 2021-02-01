c018015200da6c86dfe962e19fcefe229a6decd9
Matt Ellis <95ellismle@gmail.com>
2021-02-01 10:38:18 +0000

    Added a small deploy script to set RC files on new computer.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+# Bash set up
+cp bashrc $HOME/.bashrc
+cp bash_aliases $HOME/.bash_aliases
+
+# Vim set up
+cp vimrc $HOME/.vimrc
+mkdir $HOME/.vim
+mkdir $HOME/.vim/colors
+cp gruvbox.vim $HOME/.vim/colors
