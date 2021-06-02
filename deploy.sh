649f26c65d47bcfb5ee7d6c05968a5d29b5d7bde
Matt Ellis <95ellismle@gmail.com>
2021-06-02 17:27:38 +0100

    Editted bashrc to point to anaconda3 not miniconda3.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 # Bash set up
+echo "copying bashrc to $HOME/.bashrc"
 cp bashrc $HOME/.bashrc
+echo "copying bash_aliases to $HOME/.bash_aliases"
 cp bash_aliases $HOME/.bash_aliases
 
 # Vim set up
+echo "copying vimrc to $HOME/.vim/vimrc"
 cp vimrc $HOME/.vimrc
 mkdir -p $HOME/.vim
 mkdir -p $HOME/.vim/colors
+echo "copying gruvbox to $HOME/.vim/colors"
 cp gruvbox.vim $HOME/.vim/colors
 
 # Matplotlib setup
 mplFPATH=`python -c "import matplotlib; print(matplotlib.matplotlib_fname())"`
+echo "copying matplotlibrc to $mplFPATH"
 cp mplrc $mplFPATH
 
